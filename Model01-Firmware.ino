102e156a5d84b9f8c056b9133ca88707ae7b849d
Ryan Houston <ryanhouston83@gmail.com>
2018-06-21 13:03:21 -0400

    Replace FN mouse controls with programmer layer

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -171,10 +171,10 @@ KEYMAPS(
    ___),
 
   [FUNCTION] =  KEYMAP_STACKED
-  (___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           XXX,
-   Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
-   Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
-   Key_End,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
+  (___,      Key_F1,           Key_F2,      Key_F3,               Key_F4,                Key_F5,  XXX,
+   Key_Tab,  ___,              ___,         Key_LeftCurlyBracket, Key_RightCurlyBracket, ___,     ___,
+   Key_Home, ___,              ___,         LSHIFT(Key_9),        LSHIFT(Key_0),         ___,
+   Key_End,  Key_PrintScreen,  Key_Insert,  Key_LeftBracket,      Key_RightBracket,      ___,     ___,
    ___, Key_Delete, ___, ___,
    ___,
 
